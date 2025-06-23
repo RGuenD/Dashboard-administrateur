0400ba1254323bf44205f25ad4ca0415bb383ee4
Rahma guen <167624872+RGuenD@users.noreply.github.com>
2025-06-23 16:36:34 +0200

    Update firebaseConfig.js

--- dashboardadmin/src/services/firebaseConfig.js
+++ dashboardadmin/src/services/firebaseConfig.js
@@ -6,14 +6,14 @@ import { ref, get, set, push, update, onValue, remove } from "firebase/database"
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD9HyHGQdqVJaW0m3l_dU5Bg96ihkXuAMY",
-    authDomain: "smart-entreprise-27be8.firebaseapp.com",
-    databaseURL: "https://smart-entreprise-27be8-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "smart-entreprise-27be8",
-    storageBucket: "smart-entreprise-27be8.firebasestorage.app",
-    messagingSenderId: "60772440578",
-    appId: "1:60772440578:web:edefec3bd4b031f834a51b",
-    measurementId: "G-6YWP9ED20G"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
